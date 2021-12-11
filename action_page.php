39b2e96450e7afe05d07407718f30b11477ad01c
Jennimer <jenni.merisalo@gmail.com>
2021-12-11 16:08:48 +0200

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,6 +1,1 @@
-<?php $fname = $_POST["fname"];
-          $lname = $_POST["lname"];
-          $email = $_POST["email"];
-          $fname = $_POST["fname"];
-          $lname = $_POST["lname"];
-          $email = $_POST["email"];?>
+
